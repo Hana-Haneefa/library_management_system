54a1499b4a5770ab22f7e6d3435cf3c8cec7cffa
hana-haneefa <hana016002024@gmail.com>
2026-04-24 09:00:49 +0530

    borrow crud added

--- src/controllers/borrowController.js
+++ src/controllers/borrowController.js
@@ -1,4 +1,10 @@
-import { addBorrowData } from "../services/borrowService.js";
+import {
+  addBorrowData,
+  getAllBorrowData,
+  getBorrowDataById,
+  updateBorrowData,
+  deleteBorrowData,
+} from "../services/borrowService.js";
 
 export async function addBorrowDataController(req, res) {
   const { studentId, MonitorId, returnDate, status } = req.body;
@@ -20,5 +26,86 @@ export async function addBorrowDataController(req, res) {
     res
       .status(500)
       .json({ msg: "Error adding borrow data", error: err.message });
+  }
+}
+
+export async function getAllBorrowDataController(req, res) {
+  try {
+    const allBorrowData = await getAllBorrowData();
+    res
+      .status(200)
+      .json({ msg: "Borrow data retrieved successfully", info: allBorrowData });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ msg: "Error fetching borrow data", error: err.message });
+  }
+}
+
+export async function getBorrowDataByIdController(req, res) {
+  const { borrowId } = req.params;
+  try {
+    const borrowData = await getBorrowDataById(borrowId);
+    if (!borrowData || borrowData.brId !== parseInt(borrowId)) {
+      return res.status(404).json({ msg: "Borrow data not found" });
+    }
+    res
+      .status(200)
+      .json({ msg: "Borrow data retrieved successfully", info: borrowData });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ msg: "Error fetching borrow data", error: err.message });
+  }
+}
+
+export async function updateBorrowDataController(req, res) {
+  const { borrowId } = req.params;
+  const { status } = req.body;
+
+  if (!status || !borrowId) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  if (status !== "borrowed" && status !== "returned") {
+    return res.status(400).json({ error: "Invalid status value" });
+  }
+  try {
+    const updatedBorrowData = await updateBorrowData(borrowId, { status });
+    res.status(200).json({
+      msg: "Borrow data updated successfully",
+      info: updatedBorrowData,
+    });
+  } catch (err) {
+    if (err.message.includes("Borrow data not found with the given ID")) {
+      return res
+        .status(404)
+        .json({ msg: "Borrow data not found", error: err.message });
+    }
+    res
+      .status(500)
+      .json({ msg: "Error updating borrow data", error: err.message });
+  }
+}
+
+export async function deleteBorrowDataController(req, res) {
+  const { borrowId } = req.params;
+  if (!borrowId) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  try {
+    const deletedBorrowData = await deleteBorrowData(borrowId);
+    res.status(200).json({
+      msg: "Borrow data deleted successfully",
+      info: deletedBorrowData,
+    });
+  } catch (err) {
+    if (err.message.includes("Borrow data not found with the given ID")) {
+      return res
+        .status(404)
+        .json({ msg: "Borrow data not found", error: err.message });
+    }
+    res
+      .status(500)
+      .json({ msg: "Error deleting borrow data", error: err.message });
   }
 }
--- src/routes/borrowRoute.js
+++ src/routes/borrowRoute.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
-import { addBorrowDataController } from "../controllers/borrowController.js";
+import {
+  addBorrowDataController,
+  getAllBorrowDataController,
+  getBorrowDataByIdController,
+  updateBorrowDataController,
+  deleteBorrowDataController,
+} from "../controllers/borrowController.js";
 const router = Router();
 
 router.post("/new-borrow", addBorrowDataController);
-
+router.get("/all-borrows", getAllBorrowDataController);
+router.get("/byId/:borrowId", getBorrowDataByIdController);
+router.put("/update-borrow/:borrowId", updateBorrowDataController);
+router.delete("/delete-borrow/:borrowId", deleteBorrowDataController);
 export default router;
