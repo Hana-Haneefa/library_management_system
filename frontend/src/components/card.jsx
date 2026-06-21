e4b08c2a87bbc9268bafbc86343225c690da5698
hana-haneefa <hana016002024@gmail.com>
2026-06-21 20:08:15 +0530

    view book detail frontend done

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,11 +1,18 @@
-import bookimg from "../images/book1.jpg";
+import bookimg from "../images/book1nobg.png";
 import qrcode from "../images/icons/qrcode.png";
 import viewIcon from "../images/icons/viewIcon.png";
 import favourite from "../images/icons/heart.png";
 import favSelected from "../images/icons/heartSelected.png";
 import { useState, useEffect } from "react";
 
-function BookCard({ imageSrc, title, author, availability, genre }) {
+function BookCard({
+  imageSrc,
+  title,
+  author,
+  availability,
+  genre,
+  className = "",
+}) {
   const [isFav, setIsFav] = useState(false);
   const [label, setLabel] = useState("Read");
   const [visible, setVisible] = useState(true);
@@ -26,7 +33,9 @@ function BookCard({ imageSrc, title, author, availability, genre }) {
     setIsFav(!isFav);
   }
   return (
-    <div className="bookCard  w-50 h-72 mx-5 relative group overflow-hidden rounded-md">
+    <div
+      className={`bookCard ${className || "w-50 h-72"} w-50 h-72 mx-5 relative group overflow-hidden rounded-md`}
+    >
       {/* image div */}
       <img
         src={imageSrc || bookimg}
@@ -35,7 +44,7 @@ function BookCard({ imageSrc, title, author, availability, genre }) {
       />
 
       {/* text div */}
-      <div className="absolute bottom-0 left-0 w-full bg-white translate-y-0 group-hover:translate-y-full opacity-100 group-hover:opacity-0 transition-all duration-500 p-2 flex flex-col items-center justify-between gap-1">
+      <div className="absolute bottom-0 left-0 w-full bg-white/70 rounded-t-2xl translate-y-0 group-hover:translate-y-full opacity-100 group-hover:opacity-0 transition-all duration-500 p-2 flex flex-col items-center justify-between gap-1">
         <h3 className="text-lg font-bold">The Great Gatsby</h3>
         <p className="text-sm text-gray-600">by F. Scott Fitzgerald</p>
         <button className="bg-purple-800 w-full py-2 px-4 rounded-md text-white font-semibold hover:bg-purple-600 transition-colors duration-300">
@@ -73,7 +82,7 @@ function BookCard({ imageSrc, title, author, availability, genre }) {
         </div>
       </div>
       <div className="btn opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute bottom-2 left-1/2 transform -translate-x-1/2 w-11/12">
-        <button className="border-2 border-purple-800 w-full py-2 px-4 rounded-md bg-white/40 text-purple-800 font-semibold hover:text-purple-600 transition-colors duration-300 cursor-pointer">
+        <button className="border-2 border-purple-800 w-full py-2 px-4 rounded-md bg-white/40 text-purple-800 font-semibold hover:text-purple-600 transition-colors duration-300 cursor-pointer ml-0.5">
           <span
             style={{
               display: "inline-block",
